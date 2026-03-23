2258b67ec228c6876b2d82a66de3bdde141b5bc2
Jigyasa-prep <dasjigyasa1437@gmail.com>
2026-03-24 01:00:45 +0530

    recursion 1

---
+++ DSA/Recursion/rec1.cpp
@@ -0,0 +1,28 @@
+//Recursion-when a function calls itself until a specific condition(base condition) is met.
+/*#include<bits/stdc++.h>
+using namespace std;
+void f(int count){
+    if(count==4){
+        return;
+    }
+    cout<<"1";
+    
+    f(count+1);
+    }
+*/
+/*int cnt=0;
+void f(){
+    if(cnt==3){
+        return;
+    }
+    cout<<"1"<<endl;
+    cnt++;
+    f();
+}
+int main(){
+    f();
+    
+    return 0;
+}
+    */
+   //recursion tree-
