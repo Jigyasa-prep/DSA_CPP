2d514d66317a125a52439bd3beef0a72b6211f64
Jigyasa-prep <dasjigyasa1437@gmail.com>
2026-03-26 23:07:02 +0530

    rec2

--- DSA/Recursion/rec2.cpp
+++ DSA/Recursion/rec2.cpp
@@ -1,7 +1,7 @@
 //print name n times using recursion
 #include<bits/stdc++.h>
 using namespace std;
-void printname(string name,int n){
+/*void printname(string name,int n){
     if(n==0){
         return;
     }
@@ -15,5 +15,33 @@ int main(){
     cin>>name;
     cin>>n;
     printname(name,n);
+    return 0;
+}*/
+//print 1 to n
+/*void print1ton(int n){
+    if(n==0){
+        return;    }
+        print1ton(n-1);
+        cout<<n<< " ";
+}
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    print1ton(n);
+    return 0;
+}*/
+//print n to 1
+void printnto1(int n){
+    if(n==0){
+        return;    }
+        cout<<n<<"  ";
+        printnto1(n-1);
+}
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    printnto1(n);
     return 0;
 }
